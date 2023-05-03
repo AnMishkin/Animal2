94f8e472f058e6f00a564a0022b3d4dfa182930b
dreo <mishkin878@gmail.com>
2023-05-03 09:43:31 +0300

    finished and publishing

--- app/src/main/java/download/mishkindeveloper/AnimalSounds/MainActivity.kt
+++ app/src/main/java/download/mishkindeveloper/AnimalSounds/MainActivity.kt
@@ -8,18 +8,20 @@ import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.ads.MobileAds
+import com.google.android.ump.ConsentDebugSettings
 import com.google.android.ump.ConsentForm
 import com.google.android.ump.ConsentInformation
+import com.google.android.ump.UserMessagingPlatform
 import com.google.firebase.analytics.FirebaseAnalytics
 import download.mishkindeveloper.AnimalSounds.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
-    private val consentInformation: ConsentInformation? = null
-    private val consentForm: ConsentForm? = null
+    private lateinit var consentInformation: ConsentInformation
+    private lateinit var consentForm: ConsentForm
     private var mFirebaseAnalytics: FirebaseAnalytics? = null
        lateinit var binding: ActivityMainBinding
-//lateinit var binding :  ActivityMainBinding
+
     private lateinit var outAnimation: Animation
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,92 +30,14 @@ class MainActivity : AppCompatActivity() {
         getWindow().setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
+        )
         getSupportActionBar()?.hide();
 outAnimation = AnimationUtils.loadAnimation(this, R.anim.alfa_out)
 
+        //getConsentStatus()
+
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         MobileAds.initialize(this) { }
-        // гугл разработкик - разрешение для малолеток о просмотре рекламы
-//        val params = ConsentRequestParameters.Builder()
-//            .setTagForUnderAgeOfConsent(false)
-//            .build()
-//        consentInformation = UserMessagingPlatform.getConsentInformation(this);
-//        consentInformation.requestConsentInfoUpdate(
-//            this,
-//            params,
-//            new ConsentInformation.OnConsentInfoUpdateSuccessListener() {
-//                @Override
-//                public void onConsentInfoUpdateSuccess() {
-//                    // The consent information state was updated.
-//                    // You are now ready to check if a form is available.
-//                }
-//            },
-//            new ConsentInformation.OnConsentInfoUpdateFailureListener() {
-//                @Override
-//                public void onConsentInfoUpdateFailure(FormError formError) {
-//                    // Handle the error.
-//                }
-//            });
-//
-//        consentInformation.requestConsentInfoUpdate(
-//            this,
-//            params,
-//            new ConsentInformation.OnConsentInfoUpdateSuccessListener() {
-//                @Override
-//                public void onConsentInfoUpdateSuccess() {
-//                    // The consent information state was updated.
-//                    // You are now ready to check if a form is available.
-//                    if (consentInformation.isConsentFormAvailable()) {
-//                        loadForm();
-//                    }
-//                }
-//            },
-//            new ConsentInformation.OnConsentInfoUpdateFailureListener() {
-//                @Override
-//                public void onConsentInfoUpdateFailure(FormError formError) {
-//                    // Handle the error.
-//
-//                }
-//            });
-//
-//
-//    public void loadForm() {
-//        UserMessagingPlatform.loadConsentForm(
-//            this,
-//            new UserMessagingPlatform.OnConsentFormLoadSuccessListener() {
-//                @Override
-//                public void onConsentFormLoadSuccess(ConsentForm consentForm) {
-//                    MainActivity.this.consentForm = consentForm;
-//                    if(consentInformation.getConsentStatus() == ConsentInformation.ConsentStatus.REQUIRED) {
-//                        consentForm.show(
-//                            MainActivity.this,
-//                            new ConsentForm.OnConsentFormDismissedListener() {
-//                                @Override
-//                                public void onConsentFormDismissed(@Nullable FormError formError) {
-//                                    // Handle dismissal by reloading form.
-//                                    loadForm();
-//                                }
-//                            });
-//
-//                    }
-//
-//                }
-//            },
-//            new UserMessagingPlatform.OnConsentFormLoadFailureListener() {
-//                @Override
-//                public void onConsentFormLoadFailure(FormError formError) {
-//                    /// Handle Error.
-//    }
-
-//2я попытка по политике
-
-
-
-
-
-
-// конец - гугл разработкик - разрешение для малолеток о просмотре рекламы
 
         //политика конфиденциальности
         binding.policy.setOnClickListener {
@@ -134,6 +58,12 @@ outAnimation = AnimationUtils.loadAnimation(this, R.anim.alfa_out)
             finish()
         }
     }
+//    fun getConsentStatus(){
+//        consentInformation = UserMessagingPlatform.getConsentInformation(this)
+//
+//        ConsentDebugSettings debugSettings = new ConsentDebugSettings.Builder()
+//            .setDebugGeography(ConsentDebugSettings.)
+//    }
 }
 
 
